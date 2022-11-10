db8cd9a7018d7830f24636f6222afca2abb86589
Josuzamorano <114601348+Josuzamorano@users.noreply.github.com>
2022-11-10 12:57:23 -0300

    Add files via upload

--- boot.asm
+++ boot.asm
@@ -1,47 +1,152 @@
-ORG 0
+ORG 0x7c00
 BITS 16
-_start:
- jmp short start 
- nop 
 
-times 33 db 0
-start: 
-jmp 0x7c0:step2
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+jmp short start
+nop
+
+; FAT16 Header
+OEMIdentifier           db 'PEACHOS '
+BytesPerSector          dw 0x200
+SectorsPerCluster       db 0x80
+ReservedSectors         dw 200
+FATCopies               db 0x02
+RootDirEntries          dw 0x40
+NumSectors              dw 0x00
+MediaType               db 0xF8
+SectorsPerFat           dw 0x100
+SectorsPerTrack         dw 0x20
+NumberOfHeads           dw 0x40
+HiddenSectors           dd 0x00
+SectorsBig              dd 0x773594
+
+; Extended BPB (Dos 4.0)
+DriveNumber             db 0x80
+WinNTBit                db 0x00
+Signature               db 0x29
+VolumeID                dd 0xD105
+VolumeIDString          db 'PEACHOS BOO'
+SystemIDString          db 'FAT16   '
+
+
+start:
+    jmp 0:step2
 
 step2:
-cli
-mov ax, 0x7c0
-mov ds, ax
-mov es, ax
-mov ax, 0x00
-mov ss, ax
-mov sp, 0x7c00
+    cli 
+    mov ax, 0x00
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7c00
+    sti 
 
-sti 
-mov si, message
-call print
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
+    
 
-jmp $
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
 
-print:
-mov bx, 0
 
-.loop:
-lodsb
-cmp al, 0
-je .done 
-call print_char
-jmp .loop
+gdt_code:     
+    dw 0xffff
+    dw 0     
+    db 0      
+    db 0x9a   
+    db 11001111b
+    db 0        
 
-.done:
-ret
 
-print_char:
+gdt_data:      
+    dw 0xffff
+    dw 0      
+    db 0     
+    db 0x92   
+    db 11001111b 
+    db 0        
 
-mov ah, 0eh
-int 0x10
-ret
+gdt_end:
 
-message db 'Teodio!',0
+gdt_descriptor:
+    dw gdt_end - gdt_start-1
+    dd gdt_start
+ 
+ [BITS 32]
+ load32:
+    mov eax, 1
+    mov ecx, 100
+    mov edi, 0x0100000
+    call ata_lba_read
+    jmp CODE_SEG:0x0100000
+
+ata_lba_read:
+    mov ebx, eax, ;
+    
+    shr eax, 24
+    or eax, 0xE0 ;
+    mov dx, 0x1F6
+    out dx, al
+   
+
+
+    mov eax, ecx
+    mov dx, 0x1F2
+    out dx, al
+   
+
+  
+    mov eax, ebx 
+    mov dx, 0x1F3
+    out dx, al
+    
+
+   
+    mov dx, 0x1F4
+    mov eax, ebx ;
+    shr eax, 8
+    out dx, al
+   
+
+    
+    mov dx, 0x1F5
+    mov eax, ebx 
+    shr eax, 16
+    out dx, al
+    
+
+    mov dx, 0x1f7
+    mov al, 0x20
+    out dx, al
+
+
+.next_sector:
+    push ecx
+
+
+.try_again:
+    mov dx, 0x1f7
+    in al, dx
+    test al, 8
+    jz .try_again
+
+
+    mov ecx, 256
+    mov dx, 0x1F0
+    rep insw
+    pop ecx
+    loop .next_sector
+    
+    ret
+
 times 510-($ - $$) db 0
 dw 0xAA55
